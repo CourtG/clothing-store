a8110e2d018edc144613589cfa2c4022372499e5
Courtney Gibson <97132573+CourtG@users.noreply.github.com>
2023-01-23 15:14:11 -0500

    imported images and sass, created app.css, app.scss, Navbar.scss, worked on navbar css

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,86 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+import Cart from "../Cart/Cart";
+import { useSelector } from "react-redux";
 
-import Home from "./pages/Home/Home";
-import Product from "./pages/Product/Product";
-import Products from "./pages/Products/Products";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const products = useSelector((state) => state.cart.products);
 
-const Layout = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Outlet />
-      <Footer />
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/img/en.png" alt="" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>CAD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2">
+              Men
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">
+              Children
+            </Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link className="link" to="/">
+            STOREFRONT
+          </Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Stores
+            </Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon" onClick={() => setOpen(!open)}>
+              <ShoppingCartOutlinedIcon />
+              <span>{products.length}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {open && <Cart />}
     </div>
   );
 };
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/products:id",
-        element: <Products />,
-      },
-      {
-        path: "/product:id",
-        element: <Product />,
-      },
-    ],
-  },
-]);
-
-function App() {
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
-}
-
-export default App;
+export default Navbar;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        <div className="left"></div>
-        <div className="center"></div>
-        <div className="right"></div>
+        <div className="left">
+          <div className="item">
+            <img src="/img/en.png" alt="English language icon" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>CAD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>{" "}
+          <div className="item">
+            <Link to="/products/1">Children</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link to="/">STOREFRONT</Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link to="/">Homepage</Link>
+          </div>
+          <div className="item">
+            <Link to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Contact</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <ShoppingCartOutlinedIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
