0b370d1f407d0df0351e2880d632cec9852e919d
Courtney Gibson <97132573+CourtG@users.noreply.github.com>
2023-01-23 16:54:21 -0500

    created Card and Categories components for the homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Categories from "../../components/Categories/Categories";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../components/Slider/Slider";
 import "./Home.scss";
@@ -7,7 +8,9 @@ const Home = () => {
   return (
     <div className="home">
       <Slider />
-      <FeaturedProducts type="featured" /> <FeaturedProducts type="trending" />
+      <FeaturedProducts type="featured" />
+      <Categories />
+      <FeaturedProducts type="trending" />
     </div>
   );
 };
