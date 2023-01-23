5c55a10ddad73fd12bd1cc809d2c10f24937f097
Courtney Gibson <97132573+CourtG@users.noreply.github.com>
2023-01-23 17:04:10 -0500

    created contact banner component for homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Categories from "../../components/Categories/Categories";
+import Contact from "../../components/Contact/Contact";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../components/Slider/Slider";
 import "./Home.scss";
-
 const Home = () => {
   return (
     <div className="home">
@@ -11,6 +11,7 @@ const Home = () => {
       <FeaturedProducts type="featured" />
       <Categories />
       <FeaturedProducts type="trending" />
+      <Contact />
     </div>
   );
 };
